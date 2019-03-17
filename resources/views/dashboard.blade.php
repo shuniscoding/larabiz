638c0f487437893886967615150298f47eaaada2
shun <shunichiroid@gmail.com>
2019-03-17 16:24:59 +0900

    Creating Listings

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,37 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Dashboard<span class="pull-right"><a href="/listings/create" class="btn btn-success btn-xs">Add Listing</a></span></div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            <div class="panel-body">
+                @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
-                    <h3>Your Listings</h3>
-                    @if (count($listings))
-                        <table class="table table-striped">
+                <h3>Your Listings</h3>
+                @if (count($listings))
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Company</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        @foreach ($listings as $listing)
                             <tr>
-                                <th>Company</th>
-                                <th></th>
-                                <th></th>
+                                <td>{{ $listing->name }}</td>
+                                <td></td>
+                                <td></td>
                             </tr>
-                            @foreach ($listings as $listing)
-                                <tr>
-                                    <td>{{ $listing->name }}</td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            @endforeach
-                        </table>
-                    @endif
-                </div>
+                        @endforeach
+                    </table>
+                @endif
             </div>
         </div>
     </div>
